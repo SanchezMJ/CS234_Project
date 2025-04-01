83c69502ed83c2a3603da0d887685919c2358b5f
Michael <sanchez.michaelj@gmail.com>
2025-04-01 00:43:45 -0600

    Updated Screen/Seating
    Not completed

--- Stage3/Screen.java
+++ Stage3/Screen.java
@@ -4,10 +4,88 @@
  */
 package cs234project;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
- *
- * @author mjsanchez
+ * @author Michael Sanchez
  */
 public class Screen {
+    private String name;
+    private String type;
+    private List<String> seatList;
+    private ArrayList <Screen> screens;
+    private ArrayList <String> seating;
     
+        public Screen(String n, String t) {
+        screens = new ArrayList();
+        seating = new ArrayList();
+        this.seatList = new ArrayList<>();
+        name = n;
+        type = t;
+        if (type == "imax") {
+            seatList.addAll(Arrays.asList("IA1, IA2, IA3"));
+            seating.addAll(seatList);
+            
+        }else {
+           seatList.addAll(Arrays.asList("RA1, RA2, RA3")); 
+           seating.addAll(seatList);
+        }
+        }
+        public Screen(String n) {
+        screens = new ArrayList();
+        seating = new ArrayList();
+        this.seatList = new ArrayList<>();
+        name = n;
+        type = "Regular";
+        seatList.addAll(Arrays.asList("RA1, RA2, RA3")); 
+        seating.addAll(seatList);
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Screen> getScreens() {
+        return screens;
+    }
+
+    public void setScreens(ArrayList<Screen> screens) {
+        this.screens = screens;
+    }
+
+    //If tp is "imax", assign different seating
+    public void setSeating() {
+        
+        if (type == "imax") {
+            seatList.addAll(Arrays.asList("RA1, RA2, RA3"));
+            seating.addAll(seatList);
+            
+        }else {
+           seatList.addAll(Arrays.asList("IA1, IA2, IA3")); 
+           seating.addAll(seatList);
+        }
+    }
+    
+    public ArrayList<String> getSeating() {
+        return seating;
+    }
+    
+    @Override
+    public String toString() {
+        return "Name: " + name + " Type: " + type + "\n" + seating;
+    }
 }
--- Stage3/Seating.java
+++ Stage3/Seating.java
@@ -4,10 +4,83 @@
  */
 package cs234project;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author mjsanchez
  */
 public class Seating {
+    private String seat;
+    private double seatCost;
+    private String type;
+    //private List<String> seatList;
+    private ArrayList <String> seatSelection;
+    private ArrayList <Screen> seating;
+    
+    public Seating(String s, String t) {
+        seat = s;
+        type = t;
+        seatCost = 11.5;
+        this.seatSelection = new ArrayList();
+        this.seating = new ArrayList();
+        if (type == "imax") {
+            this.seatCost = 20.0;
+        } else {
+            this.seatCost = 11.50;
+        }
+        //this.seatList = new ArrayList<>();
+        
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public void setSeat(String seat) {
+        this.seat = seat;
+    }
+
+    public double getSeatCost() {
+        return seatCost;
+    }
+
+    public void setSeatCost(double seatCost) {
+        this.seatCost = seatCost;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void addSeatSelection(String s) {
+        seatSelection.add(s);
+    }
+    
+    public void remSeatSelection(String s) {
+        seatSelection.remove(s);
+    }
+    
+    public ArrayList<String> getSeatSelection() {
+        return seatSelection;
+    }
+    
+    public void remScreenSeats() {
+        
+    }
+    
+    public void addScreenSeats() {
+        
+    }
+    
+    @Override
+    public String toString() {
+        return "Current seat selection(s): " + seatSelection;
+    }
+    
     
 }

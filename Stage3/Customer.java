06d6df93ecd7c2303fb26475f09814e3fea241fd
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-04-02 03:36:28 -0600

    Update Customer.java

--- Stage3/Customer.java
+++ Stage3/Customer.java
@@ -4,10 +4,50 @@
  */
 package cs234project;
 
-/**
- *
- * @author mjsanchez
- */
 public class Customer {
-    
+	private String firstName; 
+	private String lastName;
+	private String dob;
+	private int  memberID;  
+	private int points; 
+	private ArrayList<Customer> members;
+	
+	public Customer() {
+		this.firstName = "";
+		this.lastName = "";
+		this.dob = "";
+		this.memberID = 0;
+		this.points = 0;  
+		this.members = new ArrayList<>();
+	}
+	
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getFirstName() {
+		return lastName;
+	}
+	
+	
+	public void setDOB(String dob) {
+		this.dob = dob;
+	}
+	
+	public String getdob() {
+		return dob;
+	}
+	
+	public void setMemberID(int memberID) {
+		this.memberID = memberID;
+	}
 }

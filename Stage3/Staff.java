7dc93d2d135b6c524915a3ede05068c7b0262362
JoBo139 <jordanbock.jb@gmail.com>
2025-04-05 16:42:04 -0600

    Update Staff.java
    Added documentation to methods.

--- Stage3/Staff.java
+++ Stage3/Staff.java
@@ -8,12 +8,15 @@ package com.mycompany.cs234project;
  *
  * @author jbock
  */
+
+//intake for staff information
 public class Staff {
     private String name;
     private String position;
     private double payRate;
     private String password;
     
+    //intialises staff info
     public Staff(String name, String position, double payRate, String password){
         this.name=name;
         this.position=position;
@@ -21,40 +24,49 @@ public class Staff {
         this.password=password;
     }
     
+    //sets name
     public void setName(String name){
         this.name=name;
     }
     
+    //gets name
     public String getName(){
         return name;
     }
     
+    //set position
     public void setPosition(String position){
         this.position=position;
     }
     
+    //gets position
     public String getPosition(){
         return position;
     }
     
+    //sets pay rate
     public void setPay(double payRate){
         this.payRate=payRate;
     }
     
+    //gets pay rate
     public double getPay(){
         return payRate;
     }
     
+    //sets password
     public void setPassword(String password){
         this.password=password;
     }
     
+    //gets password
     public String getPassword(){
         return password;
     }
     
+    //overides showEmployees() in manager to display info in a row
     @Override
     public String toString(){
-        return "Staff{name: "+ name +"' position: "+position+" pay rate: "+payRate'}';
+        return "name: "+name+" position: "+position+" pay rate: "+payRate;
     }
 }

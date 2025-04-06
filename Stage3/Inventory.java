dca6f3d1c129f5e209dc51787318bb1403286e4e
JoBo139 <jordanbock.jb@gmail.com>
2025-04-05 22:33:17 -0600

    Update Inventory.java
    added notes

--- Stage3/Inventory.java
+++ Stage3/Inventory.java
@@ -9,25 +9,30 @@ import java.util.ArrayList;
  *
  * @author jbock
  */
+//stores inventory for concession
 public class Inventory {
     private ArrayList<Concession> productList;
-    
+
+    //intialises inventory list
     public Inventory(){
     productList=new ArrayList<>();
 }
-    
+    //add product to inventory
     public void addProduct(Concession product){
         productList.add(product);
     }
-    
+
+    //removes product
     public void removeProduct(Concession product){
         productList.remove(product);
     }
-    
+
+    //gets product list
     public ArrayList<Concession> getProductList(){
         return productList;
     }
-    
+
+    //show inventory list if anything is in it
     public void showAllProducts(){
         if (productList.isEmpty()){
             System.out.println("No products in inventory.");

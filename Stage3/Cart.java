9c262e4d52d50d3adf0ad3a82cf7973150be566c
JoBo139 <jordanbock.jb@gmail.com>
2025-04-10 17:53:09 -0600

    Clear cart class added to Cart.java
    clear cart was added to empty cart after transaction

--- Stage3/Cart.java
+++ Stage3/Cart.java
@@ -9,22 +9,19 @@ import java.util.ArrayList;
  * @author jbock
  */
 public class Cart {
-    private Customer=null;
     private double total;
     //private String seat;
     private ArrayList<Concession> listOfProducts;
     private ArrayList<Seating> seatSelection;
     
     public Cart(){
-        this.customer=customer;
         this.listOfProducts=new ArrayList<>();
         this.seatSelection=new ArrayList<>();
-        this.total = total;
+        this.total = 0.0;
         //this.seat = seat;
         
     }
-
-
+    
     public Concession getProduct(Concession product){
         return product;
     }
@@ -33,10 +30,6 @@ public class Cart {
     public void addSeat(Seating seat) {
         seatSelection.add(seat);
         total = total + seat.getSeatCost();
-    }
-    
-    public double getTotal() {
-        return total;
     }
     
     public ArrayList<Seating> getSeatSelection() {
@@ -54,12 +47,25 @@ public class Cart {
     public void addProduct(Concession con) {
         listOfProducts.add(con);
         total = total + con.getPrice();
+        System.out.println("total: "+total);
     }
     
     public ArrayList<Concession> showProducts(){
         return listOfProducts;
     
-}
+    }
+ 
+    public double getTotal() {
+        System.out.println("tota: "+total);
+        return total;
+    }
+    
+    public void emptyCart(){
+        seatSelection.clear();
+        listOfProducts.clear();
+        total=0;
+    }
+    
     //Method to print itemized products in cart.  Can add for listOfProducts
     public void getCart() {
         for (Seating a:seatSelection) {

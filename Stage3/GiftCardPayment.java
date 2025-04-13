0f40ce90b9ab54e5627cb2be4fd5d41067c71fc7
JoBo139 <jordanbock.jb@gmail.com>
2025-04-13 16:47:45 -0600

    Added comments to methods

--- Stage3/GiftCardPayment.java
+++ Stage3/GiftCardPayment.java
@@ -3,31 +3,38 @@
  * @author Michael Sanchez
  */
 public class GiftCardPayment extends Payment {
+    //defines instance variables gift card number and experation date
     private String giftCardNum;
     private String expDate;
-    
+
+    //intializes variable and pulls information for payment, giftcard number, and experation date
     GiftCardPayment(double p, String gcn, String xp) {
         super(p);
         giftCardNum = gcn;
         expDate = xp;
     }       
-    
+
+    //gets gift card number
     public String getGiftCardNum() {
         return giftCardNum;
     }
 
+    //sets gift card number
     public void setGiftCardNum(String giftCardNum) {
         this.giftCardNum = giftCardNum;
     }
 
+    //gets the experation date
     public String getExpDate() {
         return expDate;
     }
 
+    //sets the experation date
     public void setExpDate(String expDate) {
         this.expDate = expDate;
     }
-    
+
+    //ovverides out put to print in a specific format
     @Override
     public void paymentDetails() {
         

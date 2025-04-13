79ebf9eae64e9aa913dd570426172ab55d390d43
JoBo139 <jordanbock.jb@gmail.com>
2025-04-13 16:31:21 -0600

    Removed package

--- Stage3/Screen.java
+++ Stage3/Screen.java
@@ -1,9 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.mycompany.cs234project;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 //import java.util.List;

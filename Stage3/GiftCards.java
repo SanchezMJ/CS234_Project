a94362d8c78e760552d5ae49a263d570fbcab0ae
JoBo139 <jordanbock.jb@gmail.com>
2025-04-13 16:50:06 -0600

    Added comments to methods

--- Stage3/GiftCards.java
+++ Stage3/GiftCards.java
@@ -5,11 +5,13 @@ import java.util.ArrayList;
  * @author mjsanchez
  */
 public class GiftCards {
+    //defines instance varables
     private long giftCardNumber;
     private String expDate;
     private double giftCardBalance;
     private ArrayList<GiftCards>giftCards;
-    
+
+    //intializes variable for gift card number, experiation date, and giftcard balance
     GiftCards(long gcn, String xp, double gcb) {
         giftCardNumber = gcn;
         expDate = xp;
@@ -17,26 +19,32 @@ public class GiftCards {
         giftCards = new ArrayList();
     }       
 
+    //gets gift card number
     public long getGiftCardNumber() {
         return giftCardNumber;
     }
 
+    //sets gift card number
     public void setGiftCardNumber(long giftCardNum) {
         this.giftCardNumber = giftCardNum;
     }
 
+    //gets experation date
     public String getExpDate() {
         return expDate;
     }
 
+    //sets experation date
     public void setExpDate(String expDate) {
         this.expDate = expDate;
     }
 
+    //gets gift card balance
     public double getGiftCardBalance() {
         return giftCardBalance;
     }
 
+    //sets gift card balance
     public void setGiftCardBalance(double giftCardBalance) {
         this.giftCardBalance = giftCardBalance;
     }

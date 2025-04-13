eb654f845e58ca9614ce6a1c8aa8a0b63cb2aa7a
JoBo139 <jordanbock.jb@gmail.com>
2025-04-13 13:26:48 -0600

    Added comments to methods

--- Stage3/Cart.java
+++ Stage3/Cart.java
@@ -9,16 +9,19 @@ import java.util.ArrayList;
  * @author jbock
  */
 public class Cart {
+    //sets instance variables
     private double total;
     private ArrayList<Concession> listOfProducts;
     private ArrayList<Seating> seatSelection;
-    
+
+    //constructor initializes product list, seat selection, and total.
     public Cart(){
         this.listOfProducts=new ArrayList<>();
         this.seatSelection=new ArrayList<>();
         this.total = 0.0;
     }
-    
+
+    //gets product from concession class.
     public Concession getProduct(Concession product){
         return product;
     }
@@ -28,11 +31,13 @@ public class Cart {
         seatSelection.add(seat);
         total = total + seat.getSeatCost();
     }
-    
+
+    //gets seat selection from seating Array List.
     public ArrayList<Seating> getSeatSelection() {
         return seatSelection;
     }
-    
+
+    //gets seat Id
     public String getSeatId(Object seat) {
         Seating s = new Seating();
         String st;
@@ -45,19 +50,24 @@ public class Cart {
         listOfProducts.add(con);
         total = total + con.getPrice();
     }
-    
+
+    //show concession list of products.
     public ArrayList<Concession> showProducts(){
         return listOfProducts;
     
     }
- 
+
+    //gets the total
     public double getTotal() {
         return total;
     }
-    
+
+    //sets the total
     public void setTotal(double m) {
         total = m;
     }
+
+    //emptys cart after payment
     public void emptyCart(){
         seatSelection.clear();
         listOfProducts.clear();

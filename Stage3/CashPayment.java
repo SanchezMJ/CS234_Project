95c6a59bdb4f438111f9fe6414a0a542a8efb874
JoBo139 <jordanbock.jb@gmail.com>
2025-04-13 13:34:52 -0600

    Added comments to methods

--- Stage3/CashPayment.java
+++ Stage3/CashPayment.java
@@ -9,21 +9,27 @@ package com.mycompany.cs234project;;
  * @author mjsanchez
  */
 public class CashPayment extends Payment{
+
+    //sets instance variable for cash amount.
     private double cashAmount;
-    
+
+    //passes the cash payment to pay for the payment amount.
     CashPayment(double p, double ca){
         super(p);
         cashAmount = ca;
     }
 
+    //gets cash amount
     public double getCashAmount() {
         return cashAmount;
     }
 
+    //sets the cash amount
     public void setCashAmount(double cashAmount) {
         this.cashAmount = cashAmount;
     }
-    
+
+    //overides out to payment to show in specific format
     @Override
     public void paymentDetails() {
         

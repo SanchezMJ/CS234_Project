c7998c9aa6342da49a9ac818f1f30080bdfe2c62
JoBo139 <jordanbock.jb@gmail.com>
2025-05-03 15:26:02 -0600

    Edited what happens when closed

--- Stage4/InventoryRemove.java
+++ Stage4/InventoryRemove.java
@@ -44,7 +44,7 @@ public class InventoryRemove extends javax.swing.JFrame {
         jInventoryRemoval = new javax.swing.JTextField();
         jInventoryRemove = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));

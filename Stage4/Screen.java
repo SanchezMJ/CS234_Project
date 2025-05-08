a9d61639bfb19bd948fa2a2c925d984a0544f0a9
Michael <sanchez.michaelj@gmail.com>
2025-05-07 23:54:17 -0600

    Screen was changed, reverted back
    Changing the seating array messed up the display.  Reverted back to the old file and its working.

--- Stage4/Screen.java
+++ Stage4/Screen.java
@@ -1,10 +1,8 @@
-package com.mycompany.cs234project;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
+package com.mycompany.cs234project;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -35,19 +33,16 @@ public class Screen {
         } else {
             this.seatCost = 11.50;
         }
-        if (type.equalsIgnoreCase("imax")) {
-            seatList.addAll(Arrays.asList(
-                "A01","A02","A03","A04","A05","A06","A07","A08","A09","A10","A11","A12","A13","A14",
-                "B01","B02","B03","B04","B05","B06","B07","B08","B09","B10","B11","B12","B13","B14",
-                "C01","C02","C03","C04","C05","C06","C07","C08","C09","C10","C11","C12","C13","C14",
-                "D01","D02","D03","D04","D05","D06","D07","D08","D09","D10","D11","D12","D13","D14",
-                "E01","E02","E03","E04","E05","E06","E07","E08","E09","E10","E11","E12","E13","E14",
-                "F01","F02","F03","F04","F05","F06","F07","F08","F09","F10","F11","F12","F13","F14",
-                "G01","G02","G03","G04","G05","G06","G07","G08","G09","G10","G11","G12","G13","G14",
-                "H01","H02","H03","H04","H05","H06","H07","H08","H09","H10","H11","H12","H13","H14"
-         ));
-         seating.addAll(seatList);
-
+        if (type == "imax") {
+            seatList.addAll(Arrays.asList("A01", "A02", "A03", "A04", "A05", "A06", "A07", "A08", "A09", "A10",
+                   "B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B09", "B10",
+                   "C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10",
+                   "D01", "D02", "D03", "D04", "D05", "D06", "D07", "D08", "D09", "D10",
+                   "E01", "E02", "E03", "E04", "E05", "E06", "E07", "E08", "E09", "E10",
+                   "F01", "F02", "F03", "F04", "F05", "F06", "F07", "F08", "F09", "F10",
+                   "G01", "G02", "G03", "G04", "G05", "G06", "G07", "G08", "G09", "G10",
+                   "H01", "H02", "H03", "H04", "H05", "H06", "H07", "H08", "H09", "H10"));
+            seating.addAll(seatList);
             
         }else {
            seatList.addAll(Arrays.asList("A01", "A02", "A03", "A04", "A05", "A06", "A07", "A08", "A09", "A10", "A11", "A12", "A13", "A14",

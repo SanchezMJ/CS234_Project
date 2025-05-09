7f5ff1249d4935935268236bcdab0263d8a5cf52
Giovanni Pernudi <pernudi.giovanni@gmail.com>
2025-05-09 00:37:05 -0600

    Update CreditCardPayment.java
    Removing package

--- Stage4/CreditCardPayment.java
+++ Stage4/CreditCardPayment.java
@@ -2,8 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.mycompany.cs234project;
-
 /**
  * CreditCardPayment represents a payment made using a credit card.
  * It extends the Payment class and includes cardholder information and card details.
